de84566966fe0177c947b49e4852a114021daba5
DEliza <32403146+Elizaddh@users.noreply.github.com>
2023-03-08 16:30:49 -0500

    Update path_rna_fastp.R

--- R/path_rna_fastp.R
+++ R/path_rna_fastp.R
@@ -1,64 +1,34 @@
 #run fastp
 #' to run trimming
 #'
-#' @param samplename
+#' @param sampleName
 #'
 #' @return
 #' @export
 #'
-#' @examples 
-run_fastp <-function(samplename){
-  library(stringr)
-
-  if (seq_tech == "PacBio" | seq_tech == "Nanopore" ){ #use custom adapters
-
-    if (endness=="PE"){
-
-      cmd <- paste0("fastp -i " ,file.path(fq.dir ,paste0( "samplename_to_sed" , filenamepattern)) , 
-                    " -I ", file.path(fq.dir , paste0("samplename_to_sed", str_replace(filenamepattern, "1", "2"))),
-                    " -o ", file.path(trim.dir , paste0("samplename_to_sed_trimmed", filenamepattern )),
-                    " -O ", file.path(trim.dir , paste0("samplename_to_sed_trimmed", str_replace(filenamepattern, "1", "2"))),
-                    "--adapter_fasta", "data/adapters.fna",
-                    " -h " , file.path(trim.dir, "samplename_to_sed.html") ,
-                    " -j " , file.path(trim.dir,  "samplename_to_sed.json"))
-
-    } else {
-      cmd <- paste0("fastp -i ",
-                    file.path(fq.dir , paste0( "samplename_to_sed" , filenamepattern)) ,
-                    " -o ",  file.path(trim.dir ,  paste0("samplename_to_sed_trimmed", filenamepattern )),
-                    " --adapter_fasta ", "data/adapters.fna", " -h " ,
-                    file.path(trim.dir,  "samplename_to_sed.html"),
-                    " -j " , file.path( "samplename_to_sed.json"))
-    }
-  
-    
+#' @examples
+run_fastp <-function(sampleName){
+  intformatch <- grep(sampleName, FileName[,1], value = F, fixed = T) #integer
+  trimmedoutfile <-  FileName[intformatch,] #trimmedoutfile$FileName1 , trimmedoutfile$Filename2
+  infile <- filenames[intformatch,]  #infile$FileName1 , infile$FileName2 #infile for SE
+  if (endness=="PE"){
+    infileoutfile <- paste0("-i ", infile$FileName1 ,   " -I ", infile$FileName2,   " -o ", trimmedoutfile["FileName1"],  " -O ", trimmedoutfile["FileName1"])
   } else {
-    if (endness=="PE"){
-
-      cmd <- paste0("fastp -i " ,
-                    file.path(fq.dir , paste0( "samplename_to_sed" , filenamepattern)) , " -I ",
-                    file.path(fq.dir , paste0("samplename_to_sed", str_replace(filenamepattern, "1", "2"))), " -o ",
-                    file.path(trim.dir , paste0("samplename_to_sed_trimmed", filenamepattern )), " -O ",
-                    file.path(trim.dir , paste0("samplename_to_sed_trimmed", str_replace(filenamepattern, "1", "2"))),   " -h " ,
-                    file.path(trim.dir, "samplename_to_sed.html") , " -j " ,
-                    file.path(trim.dir ,  "samplename_to_sed.json"))
-
-    } else {
-      print("fastp running for single end illumina running")
-      cmd <- paste0("fastp -i ",
-                    file.path(fq.dir , paste0( "samplename_to_sed" , filenamepattern)) , " -o ",
-                    file.path(trim.dir , paste0("samplename_to_sed_trimmed", filenamepattern )) , " -h " ,
-                    file.path(trim.dir,  "samplename_to_sed.html"),  " -j " ,
-                    file.path(trim.dir , "samplename_to_sed.json"))
-
-    }
+    infileoutfile <- paste0("-i ", infile, " -o ", trimmedoutfile)
   }
-  cmd <- stringr::str_replace_all(cmd, "samplename_to_sed", samplename)
 
-  if(!file.exists(file.path(trim.dir , str_replace_all("samplename_to_sed.json", "samplename_to_sed", samplename)))){
-    print("STEP 1b : running fastp")
+ logfile <- paste0( " -h " , file.path(trim.dir, paste0(sampleName, ".html")) ,  " -j " , file.path(trim.dir,  paste0(sampleName, ".json")))
+
+ #actual command
+  if (seq_tech == "PacBio" | seq_tech == "Nanopore" ){ #use custom adapters
+    cmd <- paste0("fastp " , infileoutfile,  "--adapter_fasta", "data/adapters.fna", logfile)
+  } else {
+    cmd <- paste0("fastp " , infileoutfile, logfile)
+  }
+ #file check before running command
+  if (length(list.files(trim.dir, pattern ="json")) <=0 | length(list.files(trim.dir, pattern ="trimmed")) != length(list.files(trim.dir, pattern ="json"))){
+     print("STEP 1b : running fastp")
     print(cmd)
-        system(cmd)
+    system(cmd)
   }
-
-}
+ }
