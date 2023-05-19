b876ecda7a8e069f862cc3693d52cac254119c87
DEliza <32403146+Elizaddh@users.noreply.github.com>
2023-05-19 11:19:35 -0400

    Update pathviewwrap.R

--- R/pathviewwrap.R
+++ R/pathviewwrap.R
@@ -10,6 +10,7 @@
 #' @param diff.tool :  what differential tool to to use, “DESEQ2” or “edgeR” available
 #' @param compare : what is sample/experimental design you have, paired or unpaired, as.group
 #' @param seq_tech : Illumina, pacbio or nanopore
+#' @param aligner : One of "Rhisat2" or "Rbowtie2"; Rbowtie2 can be very slow for human and eukaryotic species
 #'
 #' @importFrom stringr str_replace_all
 #' @import parallel
@@ -21,7 +22,7 @@
 #' @examples pathviewwrap(fq.dir = "~/Documents/Research/UNCC/old/mouse/fresh/mouse_raw", ref.dir = "~/Documents/Research/UNCC/old/Data/Reference/mouse", phenofile = "~/Documents/Research/UNCC/old/mouse/fresh/mouse_raw/pheno.txt", outdir="~/Documents/Research/UNCC/Research_rotation-II/newtmp/pathviewwrap/results_withoutref", endness = "SE", entity = "Mus musculus", corenum = 8, diff.tool  = "DESEQ2", compare = "unpaired", seq_tech = "Illumina")
 
 pathviewwrap <- function( ref.dir = NA, phenofile= NA, outdir="results",  entity="Mus musculus",
-                         corenum = 8,  compare="unpaired",diff.tool = "DESeq2", seq_tech="Illumina", keep_tmp = FALSE,rerun = FALSE, cacheDir = NULL ){
+                         corenum = 8,  compare="unpaired",diff.tool = "DESeq2", seq_tech="Illumina", keep_tmp = FALSE,rerun = FALSE, cacheDir = NULL , aligner){
 
 
    dirlist <- sanity_check( ref.dir ,  outdir,  entity , corenum , compare, rerun)
@@ -37,53 +38,55 @@ pathviewwrap <- function( ref.dir = NA, phenofile= NA, outdir="results",  entity
   edger.dir <- dirlist[6]
   gage.dir <- dirlist[7]
 
-  
-   if (!file.exists(phenofile)){ ###TO DO make sure reference is first aplhanumerically#
-     print("Please provide phenofile with Class information")
-   }
-   coldata <- read.table(phenofile, sep = "\t", header = T)
-   if(colnames(coldata)[ncol(coldata)]!="Class"){
-     print("Please make sure class information is in last column with colname 'Class' . ")
-   }
-   coldata$Class <- as.factor(coldata$Class)
-   SampleName <- coldata$Sample
-   filenames <- coldata[,-c(1,ncol(coldata))]
+  #duplicated codes
+  # if (!file.exists(phenofile)){ ###TO DO make sure reference is first aplhanumerically#
+  #   print("Please provide phenofile with Class information")
+  # }
+  # coldata <- read.table(phenofile, sep = "\t", header = T)
+  # if(colnames(coldata)[ncol(coldata)]!="Class"){
+  #   print("Please make sure class information is in last column with colname 'Class' . ")
+  # }
+  # coldata$Class <- as.factor(coldata$Class)
+  # SampleName <- coldata$Sample
+  # filenames <- coldata[,-c(1,ncol(coldata))]
 
-   if(is.null(dim(filenames))){
-     endness <- "SE"
-     fq.dir <-  dirname(filenames[1])
-   } else if(dim(filenames)[2] == 2){
-     endness <- "PE"
-     fq.dir <- dirname(filenames$FileName1[1])
-   
-   }
+  # if(is.null(dim(filenames))){
+  #   endness <- "SE"
+  #   fq.dir <-  dirname(filenames[1])
+  # } else if(dim(filenames)[2] == 2){
+  #   endness <- "PE"
+  #   fq.dir <- dirname(filenames$FileName1[1])
+  # 
+  # }
 
   #run the fastqc
+ 
+
+  if (!file.exists(phenofile)){ ###TO DO make sure reference is first aplhanumerically#
+    print("Please provide phenofile with Class information")
+  }
+  coldata <- read.table(phenofile, sep = "\t", header = T)
+  if(colnames(coldata)[ncol(coldata)]!="Class"){
+    print("Please make sure class information is in last column with colname 'Class' . ")
+  }
+  coldata$Class <- as.factor(coldata$Class)
+  SampleName <- coldata$Sample
+  filenames <- as.data.frame(coldata[,-c(1,ncol(coldata))])
+
+  if(dim(filenames)[2] == 1){
+    endness <- "SE"
+    fq.dir <-  dirname(filenames[1,1])
+  } else if(dim(filenames)[2] == 2){
+    endness <- "PE"
+    fq.dir <- dirname(filenames$FileName1[1])
+  }
+  
   if (!file.exists(file.path(qc.dir,"qc_heatmap.tiff"))){
     print("STEP 1 ; running fastqc")
     print("this is qc.dir")
     print(qc.dir)
     run_qc(fq.dir, qc.dir, corenum)
   }
-
-  #if (!file.exists(phenofile)){ ###TO DO make sure reference is first aplhanumerically#
-  #  print("Please provide phenofile with Class information")
-  #}
-  #coldata <- read.table(phenofile, sep = "\t", header = T)
-  #if(colnames(coldata)[ncol(coldata)]!="Class"){
-  #  print("Please make sure class information is in last column with colname 'Class' . ")
-  #}
-  #coldata$Class <- as.factor(coldata$Class)
-  #SampleName <- coldata$Sample
-  #filenames <- as.data.frame(coldata[,-c(1,ncol(coldata))])
-
-  #if(dim(filenames)[2] == 1){
-  #  endness <- "SE"
-  #  fq.dir <-  dirname(filenames[1,1])
-  #} else if(dim(filenames)[2] == 2){
-  #  endness <- "PE"
-  # fq.dir <- dirname(filenames$FileName1[1])
-  #}
 
   sampleFile <- file.path(outdir, "sampleFile.txt")
   rawfileName <- as.data.frame(sapply(filenames, function(x) basename(x)))
@@ -96,6 +99,10 @@ pathviewwrap <- function( ref.dir = NA, phenofile= NA, outdir="results",  entity
   } else{
     write.table(file=sampleFile,sep = "\t", as.data.frame( cbind(FileName[,1], FileName[,2], SampleName)), col.names = c("FileName1","FileName2", "SampleName"), quote =F ,  row.names=F)
   }
+  
+  #just in case there is random component in run_fastp
+  RNGkind("L'Ecuyer-CMRG")
+  set.seed(1)
 
   library(parallel)
   cl <- makeCluster(corenum)
@@ -103,6 +110,9 @@ pathviewwrap <- function( ref.dir = NA, phenofile= NA, outdir="results",  entity
   parSapply(cl , SampleName  ,run_fastp )
   print("the trim run is complete")
   stopCluster(cl)
+  
+  #to check if all the nodes run fine
+ # bad <- sapply(r, inherits, what = "try-error") # r<- mclappy()
 
   #make txdb from annotation
   if(!file.exists(paste0(outdir,"/", gsub(" ", "", entity), "_txdbobj"))){
@@ -115,7 +125,7 @@ pathviewwrap <- function( ref.dir = NA, phenofile= NA, outdir="results",  entity
   if(!file.exists(file.path(aligned_bam , "alltrimmedalignedobj.RDS"))){
     setwd(outdir)#make sure you delete this file before rerunning can be better
     print("STEP 3 : aligning the sequence")
-    aligned_proj <- run_qAlign(corenum, endness, sampleFile, genomeFile,geneAnnotation, ref.dir, cacheDir) #can be better??
+    aligned_proj <- run_qAlign(corenum, endness, sampleFile, genomeFile,geneAnnotation, ref.dir, cacheDir,aligner) #can be better??
   }
   else{
     aligned_proj <- readRDS(file.path(aligned_bam , "alltrimmedalignedobj.RDS"))
@@ -127,8 +137,8 @@ pathviewwrap <- function( ref.dir = NA, phenofile= NA, outdir="results",  entity
     } else{
     cnts <- as.data.frame(readRDS(file.path(outdir, "combinedcount.trimmed.RDS") ))
     }
-  print("these are samplename for cnts , cnts[, coldata$SampleName] ")
-  print(coldata$SampleName)
+  #print("these are samplename for cnts , cnts[, coldata$SampleName] ")
+  #print(coldata$SampleName)
   cnts <- cnts[, coldata$SampleName]
   if(  all(coldata$SampleName == colnames(cnts)) ){#if this then proceed
     ref <- which(coldata$Class ==  levels(coldata$Class)[1])
